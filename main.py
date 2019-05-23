1eb345ed48ce908ac80b1e85f3de034aa861f34c
Shuzhao Li <shuzhao.li@gmail.com>
2019-05-23 16:50:29 -0400

    prototype hiconet explorer gae

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def explore():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8090, debug=True)
+# [END gae_python37_app]
